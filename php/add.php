5fff5bf4e6c95e95f747a219fa4affdc32357778
600112417030@bru.ac.th <600112417030@bru.ac.th>
2019-07-12 15:41:21 +0700

    init project

---
+++ php/add.php
@@ -0,0 +1,18 @@
+<?php
+echo $_POST['user'];
+
+echo "Hello World !!!";
+$name = "Nuttawalee";
+echo "miss" . $name;
+echo sum(5,3);
+
+//function sum ($A ,$B)
+function sum($A,$B){
+    return $A + $B;
+}
+
+?>
+
+
+<!-- short term ,use one line only. -->
+<?'My name is Aern';
